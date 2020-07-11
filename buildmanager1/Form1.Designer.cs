22d4202d84fca5ea760503695899aa0a85f38d05
Darryl S <darrylshack@outlook.com>
2020-07-11 08:41:53 -0700

    more gui setup - user set archive location

--- buildmanager1/Form1.Designer.cs
+++ buildmanager1/Form1.Designer.cs
@@ -35,50 +35,51 @@
             this.button4 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(298, 65);
+            this.button1.Location = new System.Drawing.Point(12, 73);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(157, 32);
+            this.button1.Size = new System.Drawing.Size(121, 44);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Copy New Build";
+            this.button1.Text = "Get New Daily Build";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(297, 103);
+            this.button2.Location = new System.Drawing.Point(168, 73);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(157, 32);
+            this.button2.Size = new System.Drawing.Size(121, 44);
             this.button2.TabIndex = 1;
             this.button2.Text = "Delete Current Build";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(298, 141);
+            this.button3.Location = new System.Drawing.Point(301, 35);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(157, 32);
             this.button3.TabIndex = 2;
-            this.button3.Text = "Archive Build";
+            this.button3.Text = "Archive Current Build";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // listBox2
             // 
             this.listBox2.FormattingEnabled = true;
-            this.listBox2.Location = new System.Drawing.Point(12, 286);
+            this.listBox2.Location = new System.Drawing.Point(12, 373);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(280, 251);
+            this.listBox2.Size = new System.Drawing.Size(277, 108);
             this.listBox2.TabIndex = 4;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(298, 286);
+            this.button4.Location = new System.Drawing.Point(299, 373);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(157, 32);
             this.button4.TabIndex = 5;
@@ -97,24 +98,15 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 270);
+            this.label2.Location = new System.Drawing.Point(12, 357);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(86, 13);
             this.label2.TabIndex = 7;
             this.label2.Text = "Archived Build(s)";
             // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(298, 27);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(157, 32);
-            this.button5.TabIndex = 8;
-            this.button5.Text = "WIP - Install to";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(299, 324);
+            this.button6.Location = new System.Drawing.Point(299, 411);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(157, 32);
             this.button6.TabIndex = 9;
@@ -123,7 +115,7 @@
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(299, 362);
+            this.button7.Location = new System.Drawing.Point(299, 449);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(157, 32);
             this.button7.TabIndex = 10;
@@ -133,20 +125,37 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(15, 30);
+            this.listBox1.Location = new System.Drawing.Point(12, 37);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(277, 30);
             this.listBox1.TabIndex = 11;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(12, 272);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(121, 32);
+            this.button8.TabIndex = 12;
+            this.button8.Text = "Set Archive Location";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 310);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(441, 20);
+            this.textBox1.TabIndex = 13;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(470, 559);
+            this.ClientSize = new System.Drawing.Size(470, 529);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button8);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button4);
@@ -171,10 +180,11 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button7;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
