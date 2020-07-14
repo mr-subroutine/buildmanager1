0ede2cce3975eea5eca57a879628dec45305bec8
Darryl S <darrylshack@outlook.com>
2020-07-13 19:13:05 -0700

    added folder button dialog for source, destination and archive features

--- buildmanager1/Form1.Designer.cs
+++ buildmanager1/Form1.Designer.cs
@@ -37,34 +37,45 @@
             this.label2 = new System.Windows.Forms.Label();
             this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.button8 = new System.Windows.Forms.Button();
+            this.btn_archive = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btn_source = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button9 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.btn_dest = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 73);
+            this.button1.Location = new System.Drawing.Point(12, 95);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(121, 44);
+            this.button1.Size = new System.Drawing.Size(121, 53);
             this.button1.TabIndex = 0;
             this.button1.Text = "Get New Daily Build";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(168, 73);
+            this.button2.Location = new System.Drawing.Point(179, 95);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(121, 44);
+            this.button2.Size = new System.Drawing.Size(110, 24);
             this.button2.TabIndex = 1;
             this.button2.Text = "Delete Current Build";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(301, 35);
+            this.button3.Location = new System.Drawing.Point(179, 124);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(157, 32);
+            this.button3.Size = new System.Drawing.Size(110, 24);
             this.button3.TabIndex = 2;
             this.button3.Text = "Archive Current Build";
             this.button3.UseVisualStyleBackColor = true;
@@ -72,14 +83,14 @@
             // listBox2
             // 
             this.listBox2.FormattingEnabled = true;
-            this.listBox2.Location = new System.Drawing.Point(12, 373);
+            this.listBox2.Location = new System.Drawing.Point(15, 373);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(277, 108);
+            this.listBox2.Size = new System.Drawing.Size(274, 147);
             this.listBox2.TabIndex = 4;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(299, 373);
+            this.button4.Location = new System.Drawing.Point(299, 411);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(157, 32);
             this.button4.TabIndex = 5;
@@ -89,11 +100,11 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Location = new System.Drawing.Point(12, 14);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.Size = new System.Drawing.Size(73, 13);
             this.label1.TabIndex = 6;
-            this.label1.Text = "Current Build";
+            this.label1.Text = "Source Folder";
             // 
             // label2
             // 
@@ -106,7 +117,7 @@
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(299, 411);
+            this.button6.Location = new System.Drawing.Point(299, 449);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(157, 32);
             this.button6.TabIndex = 9;
@@ -115,45 +126,139 @@
             // 
             // button7
             // 
-            this.button7.Location = new System.Drawing.Point(299, 449);
+            this.button7.Location = new System.Drawing.Point(299, 487);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(157, 32);
             this.button7.TabIndex = 10;
             this.button7.Text = "Duplicate Archive";
             this.button7.UseVisualStyleBackColor = true;
             // 
-            // listBox1
+            // btn_archive
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(12, 37);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(277, 30);
-            this.listBox1.TabIndex = 11;
-            // 
-            // button8
-            // 
-            this.button8.Location = new System.Drawing.Point(12, 272);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(121, 32);
-            this.button8.TabIndex = 12;
-            this.button8.Text = "Set Archive Location";
-            this.button8.UseVisualStyleBackColor = true;
+            this.btn_archive.Location = new System.Drawing.Point(179, 307);
+            this.btn_archive.Name = "btn_archive";
+            this.btn_archive.Size = new System.Drawing.Size(110, 24);
+            this.btn_archive.TabIndex = 12;
+            this.btn_archive.Text = "Set Archive Location";
+            this.btn_archive.UseVisualStyleBackColor = true;
+            this.btn_archive.Click += new System.EventHandler(this.button8_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(15, 310);
+            this.textBox1.Location = new System.Drawing.Point(15, 281);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(441, 20);
+            this.textBox1.Size = new System.Drawing.Size(274, 20);
             this.textBox1.TabIndex = 13;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 30);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(277, 20);
+            this.textBox2.TabIndex = 14;
+            // 
+            // btn_source
+            // 
+            this.btn_source.Location = new System.Drawing.Point(299, 24);
+            this.btn_source.Name = "btn_source";
+            this.btn_source.Size = new System.Drawing.Size(157, 31);
+            this.btn_source.TabIndex = 15;
+            this.btn_source.Text = "Set Build Source";
+            this.btn_source.UseVisualStyleBackColor = true;
+            this.btn_source.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(299, 373);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(157, 32);
+            this.button9.TabIndex = 16;
+            this.button9.Text = "Make As Current Build";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(12, 69);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(277, 20);
+            this.textBox3.TabIndex = 17;
+            // 
+            // btn_dest
+            // 
+            this.btn_dest.Location = new System.Drawing.Point(299, 63);
+            this.btn_dest.Name = "btn_dest";
+            this.btn_dest.Size = new System.Drawing.Size(157, 31);
+            this.btn_dest.TabIndex = 18;
+            this.btn_dest.Text = "Set Build Destination";
+            this.btn_dest.UseVisualStyleBackColor = true;
+            this.btn_dest.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 53);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Destination Folder";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 265);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 13);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Archive Location";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Location = new System.Drawing.Point(301, 265);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(157, 66);
+            this.groupBox1.TabIndex = 21;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Auto Archive";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 29);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(39, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "On";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(112, 29);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(39, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Off";
+            this.radioButton2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(470, 529);
+            this.ClientSize = new System.Drawing.Size(470, 564);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_dest);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.btn_source);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button8);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btn_archive);
             this.Controls.Add(this.button7);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.label2);
@@ -166,6 +271,8 @@
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Build Manager ver 0.1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -182,9 +289,19 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button btn_archive;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btn_source;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button btn_dest;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
     }
 }
 
--- buildmanager1/Form1.cs
+++ buildmanager1/Form1.cs
@@ -12,9 +12,56 @@ namespace buildmanager1
 {
     public partial class Form1 : Form
     {
+        protected string newBuildLoc;
+        protected string archiveBuildLoc;
+        
+        // browser dialog for build copy
+        FolderBrowserDialog fbd = new FolderBrowserDialog();
+        FolderBrowserDialog fbd2 = new FolderBrowserDialog();
+        FolderBrowserDialog fbd3 = new FolderBrowserDialog();
+
+        // set default path for build source on a server
+        // <--------------------HERE-------------------->
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            fbd.RootFolder = Environment.SpecialFolder.Desktop;
+            fbd.Description = "Select A Folder Destination For Builds";
+            fbd.ShowNewFolderButton = false;
+
+            if(fbd.ShowDialog() == DialogResult.OK)
+            {
+                textBox2.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            fbd2.RootFolder = Environment.SpecialFolder.Desktop;
+            fbd2.Description = "Select A Folder Destination For Builds";
+            fbd2.ShowNewFolderButton = false;
+
+            if (fbd2.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = fbd2.SelectedPath;
+            }
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            fbd3.RootFolder = Environment.SpecialFolder.Desktop;
+            fbd3.Description = "Select A Folder Destination For Builds";
+            fbd3.ShowNewFolderButton = false;
+
+            if (fbd3.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = fbd3.SelectedPath;
+            }
         }
     }
 }
